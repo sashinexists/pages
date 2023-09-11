7e03418b83fcb0dfc24458fb75d7aeaa6808c9fc
sashiexists <myself@sashinexists.com>
2023-09-12 08:32:29 +1000

    add document struct

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,88 @@ use std::fs;
 mod ui;
 use ui::*;
 fn main() {
-    let view = column()
-        .push(row().push(text("hello").add_style(Style::TextColor(Color::Blue))))
-        .push(row().push(text("my").add_style(Style::BackgroundColor(Color::Red))))
+    let document = (Document::new("Test Page")).add_style(Style::BackgroundColor(Color::Black));
+
+    let content = column()
+        .add_style(Style::Width(Unit::Percent(90.0)))
+        .add_style(Style::Center)
+        .add_style(Style::BackgroundColor(Color::Grey))
+        .add_style(Style::TextColor(Color::White))
+        .add_style(Style::Rounded(Unit::Px(15)))
+        .add_style(Style::Padding(Unit::Px(30)))
+        .push(row().push(text("hello").add_style(Style::TextColor(Color::Yellow))))
+        .push(row().push(text("my").add_style(Style::BackgroundColor(Color::Orange))))
         .push(row().push(text("name")))
         .push(row().push(text("is").add_style(Style::BackgroundColor(Color::Blue))))
         .push(row().push(text("chara").add_style(Style::TextColor(Color::Red))));
 
-    let output = view.to_html();
-    dbg!(view);
+    let document = document.push(content);
+
+    let output = document.to_html();
+    dbg!(document);
     fs::write("index.html", output).expect("Failed to write to index.html");
+}
+
+#[derive(Debug, Clone)]
+struct Document {
+    title: String,
+    styles: Vec<Style>,
+    content: Vec<Element>,
+}
+
+impl Document {
+    fn new(title: &str) -> Self {
+        Self {
+            title: title.to_string(),
+            styles: Vec::new(),
+            content: Vec::new(),
+        }
+    }
+
+    fn push(mut self, element: Element) -> Self {
+        self.content.push(element);
+        self
+    }
+
+    fn get_inline_style_string(&self) -> String {
+        self.styles
+            .iter()
+            .fold(String::new(), |style_string, style| {
+                style_string + &style.to_string()
+            })
+    }
+
+    fn get_elements_html(&self) -> String {
+        self.content.iter().fold("".to_string(), |output, element| {
+            output + &element.to_html()
+        })
+    }
+
+    pub fn add_style(mut self, style: Style) -> Self {
+        self.styles.push(style);
+        self
+    }
+
+    pub fn add_styles(mut self, styles: Vec<Style>) -> Self {
+        self.styles.extend(styles);
+        self
+    }
+
+    fn to_html(&self) -> String {
+        format!(
+            "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>{}</title>
+</head>
+<body style=\"{}\">
+{}
+</body>
+</html>",
+            self.title,
+            self.get_inline_style_string(),
+            self.get_elements_html()
+        )
+    }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -3,28 +3,30 @@ use std::fmt::Debug;
 #[derive(Debug)]
 pub struct PushError;
 
-#[derive(Debug, Clone )]
+#[derive(Debug, Clone)]
 pub struct Element {
-    content:ElementContent,
+    content: ElementContent,
     meta: ElementMetaData,
 }
-impl Element{ 
-
+impl Element {
     pub fn to_html(&self) -> String {
-        println!("{}",&self.get_inline_style_string());
-        self.content.to_html().replace("~~styles~~", &self.get_inline_style_string())
+        println!("{}", &self.get_inline_style_string());
+        self.content
+            .to_html()
+            .replace("~~styles~~", &self.get_inline_style_string())
     }
 
-    fn get_inline_style_string(&self)->String {
-        self.meta.styles.iter().fold(
-            String::new(), |style_string, style| {
+    fn get_inline_style_string(&self) -> String {
+        self.meta
+            .styles
+            .iter()
+            .fold(String::new(), |style_string, style| {
                 style_string + &style.to_string()
-            }
-        )
+            })
     }
 
-    pub fn push(&mut self, element:Element) -> Self {
-        match &mut self.content{
+    pub fn push(&mut self, element: Element) -> Self {
+        match &mut self.content {
             ElementContent::Column(column) => {
                 column.elements.push(element);
                 self.clone()
@@ -32,11 +34,11 @@ impl Element{
             ElementContent::Row(row) => {
                 row.elements.push(element);
                 self.clone()
-            },
-            _ => panic!("Tried to push to an unpushable element. {:?}", self)
+            }
+            _ => panic!("Tried to push to an unpushable element. {:?}", self),
         }
     }
-    
+
     pub fn add_style(mut self, style: Style) -> Self {
         self.meta.add_style(style);
         self
@@ -47,7 +49,7 @@ impl Element{
         self
     }
 }
-#[derive(Debug, Clone )]
+#[derive(Debug, Clone)]
 pub enum ElementContent {
     Column(Column),
     Row(Row),
@@ -112,29 +114,33 @@ impl Attribute {
 
 #[derive(Clone, Debug)]
 pub enum Style {
-    Rounded(u32),
+    Rounded(Unit),
     RoundedEach(Corners),
-    Margin(u32),
+    Margin(Unit),
     MarginEach(Sides),
-    Padding(u32),
+    Padding(Unit),
     PaddingEach(Sides),
     BackgroundColor(Color),
     TextColor(Color),
+    Center,
+    Width(Unit),
+    Height(Unit),
 }
 
 impl Style {
-    fn to_string(&self) -> String {
+    pub fn to_string(&self) -> String {
         match self{
-            Self::Rounded(px) => format!("border-radius:{}px; ", px),
-            Self::RoundedEach(corners) => format!("border-top-left-radius:{}px; border-top-right-radius:{}px; border-bottom-left-radius:{}px; border-bottom-right-radius:{}px; ", corners.top_left, corners.top_right, corners.bottom_left, corners.bottom_right),
-            Self::Margin(px) => format!("margin:{}px; ", px),
-            Self::MarginEach(sides) => format!("margin-top:{}px; margin-bottom:{}px; margin-right:{}px; margin-left:{}px; ", sides.top, sides.bottom, sides.right, sides.left),
-            Self::Padding(px) => format!("padding:{}px; ", px),
-            Self::PaddingEach(sides) => format!("padding-top:{}px; padding-bottom:{}px; padding-right:{}px; padding-left:{}px; ", sides.top, sides.bottom, sides.right, sides.left),
-            Self::BackgroundColor(color) => format!("background-color:{}; ", color.to_string()),
-            Self::TextColor(color) => format!("color:{}; ", color.to_string())
-            
-            
+            Self::Rounded(unit) => format!("border-radius:{unit};"),
+            Self::RoundedEach(corners) => format!("border-top-left-radius:{};border-top-right-radius:{};border-bottom-left-radius:{};border-bottom-right-radius:{};", corners.top_left, corners.top_right, corners.bottom_left, corners.bottom_right),
+            Self::Margin(unit) => format!("margin:{};", unit),
+            Self::MarginEach(sides) => format!("margin-top:{};margin-bottom:{};margin-right:{};margin-left:{};", sides.top, sides.bottom, sides.right, sides.left),
+            Self::Padding(unit) => format!("padding:{unit};"),
+            Self::PaddingEach(sides) => format!("padding-top:{};padding-bottom:{};padding-right:{};padding-left:{};", sides.top, sides.bottom, sides.right, sides.left),
+            Self::BackgroundColor(color) => format!("background-color:{};", color.to_string()),
+            Self::TextColor(color) => format!("color:{};", color.to_string()),
+            Self::Center => format!("margin:auto;"),
+            Self::Height(unit) => format!("height:{unit};" ),
+            Self::Width(unit) => format!("width:{unit};" )
         }
     }
 }
@@ -143,31 +149,45 @@ impl Style {
 pub enum Color {
     Red,
     Blue,
+    Yellow,
+    Green,
+    Orange,
+    Purple,
+    Black,
+    White,
+    Grey,
 }
 
 impl Color {
-    fn to_string(&self) ->String {
+    fn to_string(&self) -> String {
         match self {
             Self::Red => "red".to_owned(),
-            Self::Blue => "blue".to_owned()
+            Self::Blue => "blue".to_owned(),
+            Self::Yellow => "yellow".to_owned(),
+            Self::Green => "green".to_owned(),
+            Self::Orange => "orange".to_owned(),
+            Self::Purple => "purple".to_owned(),
+            Self::Black => "black".to_owned(),
+            Self::White => "white".to_owned(),
+            Self::Grey => "grey".to_owned(),
         }
     }
 }
 
 #[derive(Clone, Debug)]
 pub struct Sides {
-    top: u32,
-    bottom: u32,
-    left: u32,
-    right: u32,
+    top: Unit,
+    bottom: Unit,
+    left: Unit,
+    right: Unit,
 }
 
 #[derive(Clone, Debug)]
 pub struct Corners {
-    top_left: u32,
-    top_right: u32,
-    bottom_left: u32,
-    bottom_right: u32,
+    top_left: Unit,
+    top_right: Unit,
+    bottom_left: Unit,
+    bottom_right: Unit,
 }
 
 #[derive(Debug, Clone)]
@@ -178,10 +198,10 @@ pub struct Row {
 impl Row {
     pub fn new() -> Element {
         Element {
-            content: ElementContent::Row(Self{
-                elements: Vec::new()
+            content: ElementContent::Row(Self {
+                elements: Vec::new(),
             }),
-            meta: ElementMetaData::new()
+            meta: ElementMetaData::new(),
         }
     }
 
@@ -197,7 +217,7 @@ impl El for Row {
             .elements
             .iter()
             .fold("".to_string(), |acc, element| acc + &element.to_html());
-        format!("<div {{attributes}} class={{classes}} style={{styles}}>{elements}</div>")
+        format!("<div {{attributes}} class={{classes}} style=~~styles~~>{elements}</div>")
     }
 }
 
@@ -209,10 +229,10 @@ pub struct Column {
 impl Column {
     pub fn new() -> Element {
         Element {
-            content: ElementContent::Column(Self{
-                elements: Vec::new()
+            content: ElementContent::Column(Self {
+                elements: Vec::new(),
             }),
-            meta: ElementMetaData::new()
+            meta: ElementMetaData::new(),
         }
     }
 
@@ -228,7 +248,7 @@ impl El for Column {
             .elements
             .iter()
             .fold("".to_string(), |acc, element| acc + &element.to_html());
-        format!("<div {{attributes}} class={{classes}} style={{styles}}>{elements}</div>")
+        format!("<div {{attributes}} class={{classes}} style=~~styles~~>{elements}</div>")
     }
 }
 
@@ -240,8 +260,10 @@ pub struct Text {
 impl Text {
     pub fn new(content: &str) -> Element {
         Element {
-            content: ElementContent::Text(Self{content: content.to_string()}),
-            meta: ElementMetaData::new()
+            content: ElementContent::Text(Self {
+                content: content.to_string(),
+            }),
+            meta: ElementMetaData::new(),
         }
     }
 }
@@ -265,7 +287,7 @@ pub struct Button {
 impl El for Button {
     fn to_html(&self) -> String {
         format!(
-            "<button href=\"{}\" {{attributes}} class={{classes}} style={{style}}>{}</button>",
+            "<button href=\"{}\" {{attributes}} class={{classes}} style=~~styles~~>{}</button>",
             self.on_press,
             self.label.to_html()
         )
@@ -287,7 +309,7 @@ pub trait El: Debug {
 impl El for Link {
     fn to_html(&self) -> String {
         format!(
-            "<a href=\"{}\" class={{classes}} style={{styles}} {{attributes}}>{}</a>",
+            "<a href=\"{}\" class={{classes}} style=~~styles~~ {{attributes}}>{}</a>",
             self.target,
             self.label.to_html()
         )
@@ -295,8 +317,7 @@ impl El for Link {
     }
 }
 
-
-pub fn column()-> Element {
+pub fn column() -> Element {
     Column::new()
 }
 
@@ -304,6 +325,35 @@ pub fn row() -> Element {
     Row::new()
 }
 
-pub fn text(text:&str) -> Element {
+pub fn text(text: &str) -> Element {
     Text::new(text)
+}
+
+#[derive(Debug, Clone)]
+pub enum Unit {
+    Px(u32),
+    Em(f32),
+    Rem(f32),
+    Percent(f32),
+}
+
+impl std::fmt::Display for Unit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Unit::Px(px) => write!(f, "{}px", px),
+            Unit::Em(em) => write!(f, "{}em", em),
+            Unit::Rem(rem) => write!(f, "{}rem", rem),
+            Unit::Percent(percent) => write!(f, "{}%", percent),
+        }
+    }
+}
+impl Unit {
+    fn to_string(&self) -> String {
+        match self {
+            Unit::Px(px) => format!("{px}px"),
+            Unit::Em(em) => format!("{em}em"),
+            Unit::Rem(rem) => format!("{rem}rem"),
+            Unit::Percent(percent) => format!("{percent}%"),
+        }
+    }
 }
